8a0cfd49c5dbc965a32d7c3f488de64f6ed8ee4c
Martin Kodovský <martin.kodovsky@firma.seznam.cz>
2017-09-13 11:54:52 +0200

    zamykani example

--- Rx/v2/examples/my_delete/main.cpp
+++ Rx/v2/examples/my_delete/main.cpp
@@ -1,5 +1,6 @@
 #include "rxcpp/rx.hpp"
 
+#include <iostream>
 #include <mutex>
 #include <condition_variable>
 
@@ -21,73 +22,143 @@ std::condition_variable cv;
 class Callback
 {
 public:
-    Callback() {
-        std::cout << "Callback created " << this << std::endl;
-    }
-    Callback(const Callback &) = delete;
-    Callback(Callback&&) = delete;
+	Callback() {
+		std::cout << "Callback created " << this << std::endl;
+	}
+	Callback(const Callback &) = delete;
+	Callback(Callback&&) = delete;
 
-    void onCalled(int d) {
-        std::cout << d << std::endl;
-    }
+	void onCalled(int d) {
+		std::cout << d << std::endl;
+	}
 };
 
 class CWrapper {
 
 public:
 
-    CWrapper() : cb(new Callback()) {}
+	CWrapper() : cb(new Callback()) {}
 
-    ~CWrapper() { delete cb; cb = nullptr; }
+	~CWrapper() { delete cb; cb = nullptr; }
 
-    Callback *cb;
+	Callback *cb;
 };
+
+
+template<typename T>
+class safe_observable {
+public:
+
+	safe_observable(rx::observable<T> t) : in_progress(false) {
+		//obs = t | rxo::finally([]() { if(in_progress) });
+	}
+
+
+	// void subscribe();
+
+	// void subscribe(std::function<void(T)>);
+
+	// void subscribe(std::function<void(T)>, std::function<void(std::exception_ptr)>);
+
+	// void subscribe(std::function<void(T)>, std::function<void(std::exception_ptr)>, std::function<void()>) {}
+
+private:
+	rx::observable<T> obs;
+	std::recursive_mutex mtx;
+	bool in_progress;
+};
+
 
 int main()
 {
 
-    using tt = std::chrono::high_resolution_clock;
-    using ms = std::chrono::milliseconds;
-    using fsec = std::chrono::duration<float>;
+	std::mutex simMtx;
+	std::condition_variable simCondition;
+	bool simTrigger = false;
 
-    std::recursive_mutex mutex;
-    CWrapper *cw = new CWrapper();
+	using tt = std::chrono::high_resolution_clock;
+	using ms = std::chrono::milliseconds;
+	using fsec = std::chrono::duration<float>;
 
-    auto observable = rx::observable<>::create<int>(
-        [](rx::subscriber<int> s) {
+	std::mutex mutex;
+	CWrapper *cw = new CWrapper();
 
-            //s.add(rx::make_subscription([&]() { cv.notify_all(); }));
-            //std::mutex mtx;
-            //std::unique_lock<std::mutex> lck(mtx);
+	auto observable = rx::observable<>::create<int>(
+		[](rx::subscriber<int> s) {
 
-            auto t0 = tt::now();
-            //cv.wait_for(lck,std::chrono::seconds(5));
-            std::this_thread::sleep_for(ms(5000));
-            auto t1 = tt::now();
+			//s.add(rx::make_subscription([&]() { cv.notify_all(); }));
+			auto t0 = tt::now();
+			std::this_thread::sleep_for(ms(2000));
+			auto t1 = tt::now();
 
-            fsec diff = t1 - t0;
+			fsec diff = t1 - t0;
 
-            s.on_next(std::chrono::duration_cast<ms>(diff).count());
+			s.on_next(std::chrono::duration_cast<ms>(diff).count());
 
-            s.on_completed();
-        }
-    ) | rxo::subscribe_on(rx::observe_on_new_thread());
+			s.on_completed();
+		}
+	) | rxo::finally([&](){
+			std::lock_guard<std::mutex> crit(mutex);
+			std::cout << "finally\n"; }) |
+		rxo::subscribe_on(rx::observe_on_new_thread());
 
+	auto sub = observable.subscribe([&](int d) {
 
-    auto sub = observable.subscribe([&cw](int d) {
-            std::cout << "callback about to be triggered with " << d << "[" << cw->cb << "]" << std::endl;
-            std::this_thread::sleep_for(ms(500));
-            cw->cb->onCalled(d); });
+			std::lock_guard<std::mutex> crit(mutex);
+			// uvolni cekajici vlakno, aby mohlo smazat captured promennou pod rukama
+			std::unique_lock<std::mutex> lck(simMtx);
+			simTrigger = true;
+			lck.unlock();
+			simCondition.notify_all();
 
-    delete cw;
-    cw = nullptr;
+			// pockej, aby mazaci vlakno melo cas smazat objekt
+			std::this_thread::sleep_for(ms(500));
 
+			std::cout << "segfault?\n";
+			cw->cb->onCalled(d);
+		}
+		);
 
-    std::cout <<  "callback gone [0]\n";// << cw->cb << "]\n";
+	//#############case 1
+	//sub.unsubscribe(); // callback se nikdy neprovola(nestihne to)
 
-    //sub.unsubscribe();
+	// simuluj mazani na jinem vlakne
+	auto deleter = rx::observable<>::create<int>(
+		[&](rx::subscriber<int> s) {
 
-    std::this_thread::sleep_for(ms(6000));
+			//pockej nez se zacne provadet lambda v subscribci
+			{
+				std::unique_lock<std::mutex> lck(simMtx);
+				simCondition.wait(
+					lck,
+					[&]
+					{
+						return simTrigger;
+					}
+				);
+			}
 
-    return 0;
+			//###############case 2
+			sub.unsubscribe(); // pokud callback probiha, je zamkly, toto je blokujici
+			delete cw;
+			cw = nullptr;
+
+			s.on_completed();
+		}) | rxo::finally([&](){
+			// uvolni cekajici vlakno
+			std::unique_lock<std::mutex> lck(simMtx);
+			simTrigger = true;
+			lck.unlock();
+			simCondition.notify_all();
+			}) | rxo::subscribe_on(rx::observe_on_new_thread());
+
+	auto dd = deleter.subscribe();
+
+	std::this_thread::sleep_for(ms(6000));
+
+	std::cout <<  "Test passed OK!\n";// << cw->cb << "]\n";
+
+	dd.unsubscribe(); // deleter uz neni treba, tak ho interruptni (kvuli ####case 1)
+
+	return 0;
 }
